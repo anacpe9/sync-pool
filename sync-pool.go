5d8eeffd713bf3dd4dcd6eed90c6e40aa833d71b
Anucha Nualsi <ana.cpe9@gmail.com>
2025-02-22 09:56:50 +0700

    feat: initialize a nested pointer

--- sync-pool.go
+++ sync-pool.go
@@ -16,35 +16,69 @@ type Pool[T any] struct {
 	pool *sync.Pool
 }
 
+func initializeNestedPointer(ptr interface{}) {
+	v := reflect.ValueOf(ptr)
+
+	// // Use it internally and send pointers.
+	// if v.Kind() != reflect.Ptr {
+	// 	panic("expected a pointer")
+	// }
+
+	for v.Kind() == reflect.Ptr {
+		elemType := v.Type().Elem()
+		if v.IsNil() {
+			v.Set(reflect.New(v.Type().Elem()))
+		} else if elemType.Kind() == reflect.Slice {
+			v.Elem().Set(reflect.MakeSlice(elemType, 0, 0))
+		}
+		v = v.Elem()
+	}
+}
+
 func GetPool[T any]() *Pool[T] {
 	// this function should not call frequency.
 	// We allow to allocated one of T to reflect type
 	// and put back to pool
-	t := new(T)
+
+	newType := func() *T {
+		tt := new(T)
+		initializeNestedPointer(tt)
+		return tt
+	}
+
+	t := newType()
 	key := reflect.TypeOf(t).String()
 	pool := genericTypeMap[key]
 
-	if pool != nil {
-		p := pool.(*Pool[T])
-		p.Put(t)
+	if pool == nil {
+		newPool := &Pool[T]{
+			pool: &sync.Pool{
+				New: func() interface{} { return newType() },
+			},
+		}
 
-		return p
+		genericTypeMap[key] = newPool
+		pool = genericTypeMap[key]
 	}
 
-	newPool := &Pool[T]{
-		pool: &sync.Pool{
-			New: func() interface{} { return new(T) },
-		},
-	}
+	p := pool.(*Pool[T])
+	p.Put(t)
 
-	newPool.Put(t)
-	genericTypeMap[key] = newPool
-	return newPool
+	return p
 }
 
 // Get returns an arbitrary item from the pool.
 func (p *Pool[T]) Get() *T {
-	return p.pool.Get().(*T)
+	itm := p.pool.Get().(*T)
+	// // Try to use drainPool
+	// // But we not retrieve nil
+	// // So this statement we ignore
+	//
+	// if itm == nil {
+	// 	itm = new(T)
+	// }
+
+	return itm
 }
 
 // Put places an item in the pool
--- sync-pool_test.go
+++ sync-pool_test.go
@@ -4,119 +4,147 @@ import (
 	"testing"
 )
 
+const logTpl = "got: %d, want: %d"
+
 func TestPool(t *testing.T) {
 	type Foo struct{ x int }
 	type Bar struct{ y int }
 
-	fooPool_01 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
-	fooPool_02 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
-	fooPool_03 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
+	fooPool01 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
+	fooPool02 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
+	fooPool03 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
 
-	barPool_01 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
-	barPool_02 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
-	barPool_03 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
+	barPool01 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
+	barPool02 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
+	barPool03 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
 
-	foo_01 := fooPool_01.Get()
-	foo_02 := fooPool_02.Get()
-	foo_03 := fooPool_03.Get()
+	foo01 := fooPool01.Get()
+	foo02 := fooPool02.Get()
+	foo03 := fooPool03.Get()
 
-	bar_01 := barPool_01.Get()
-	bar_02 := barPool_02.Get()
-	bar_03 := barPool_03.Get()
+	bar01 := barPool01.Get()
+	bar02 := barPool02.Get()
+	bar03 := barPool03.Get()
 
 	// new instant all value should be 0
-	if got, want := foo_01.x, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	if got, want := foo01.x, 0; got != want {
+		t.Errorf(logTpl, got, want)
 	}
-	if got, want := foo_02.x, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	if got, want := foo02.x, 0; got != want {
+		t.Errorf(logTpl, got, want)
 	}
-	if got, want := foo_03.x, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
-	}
-
-	if got, want := bar_01.y, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
-	}
-	if got, want := bar_02.y, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
-	}
-	if got, want := bar_03.y, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	if got, want := foo03.x, 0; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	foo_01.x = 1
-	foo_02.x = 2
-	foo_03.x = 3
-	bar_01.y = 11
-	bar_02.y = 12
-	bar_03.y = 13
-	val_x_01 := foo_01.x
-	val_x_02 := foo_02.x
-	val_x_03 := foo_03.x
-	val_y_01 := bar_01.y
-	val_y_02 := bar_02.y
-	val_y_03 := bar_03.y
+	if got, want := bar01.y, 0; got != want {
+		t.Errorf(logTpl, got, want)
+	}
+	if got, want := bar02.y, 0; got != want {
+		t.Errorf(logTpl, got, want)
+	}
+	if got, want := bar03.y, 0; got != want {
+		t.Errorf(logTpl, got, want)
+	}
+
+	foo01.x = 1
+	foo02.x = 2
+	foo03.x = 3
+	bar01.y = 11
+	bar02.y = 12
+	bar03.y = 13
+	valX01 := foo01.x
+	valX02 := foo02.x
+	valX03 := foo03.x
+	valY01 := bar01.y
+	valY02 := bar02.y
+	valY03 := bar03.y
 
 	// this prove the sync.Pool must be the same by push back
 	// and pull new is should be the same one value
-	fooPool_03.Put(foo_01)
-	new_x_01 := fooPool_02.Get()
-	if got, want := new_x_01.x, val_x_01; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	fooPool03.Put(foo01)
+	newX01 := fooPool02.Get()
+	if got, want := newX01.x, valX01; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	fooPool_02.Put(foo_02)
-	new_x_02 := fooPool_03.Get()
-	if got, want := new_x_02.x, val_x_02; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	fooPool02.Put(foo02)
+	newX02 := fooPool03.Get()
+	if got, want := newX02.x, valX02; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	fooPool_01.Put(foo_03)
-	new_x_03 := fooPool_03.Get()
-	if got, want := new_x_03.x, val_x_03; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	fooPool01.Put(foo03)
+	newX03 := fooPool03.Get()
+	if got, want := newX03.x, valX03; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	fooPool_04 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
-	fooPool_04.Get()             // pop new(T)
-	fooPool_02.Put(new_x_03)
-	new_x_x3 := fooPool_04.Get()
-	if got, want := new_x_x3.x, val_x_03; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	fooPool04 := GetPool[Foo]() // when call GetPoo[T], it's call new (T) every time
+	fooPool04.Get()             // pop new(T)
+	fooPool02.Put(newX03)
+	newXx3 := fooPool04.Get()
+	if got, want := newXx3.x, valX03; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
 	// ------------------------------------------------
-	barPool_03.Put(bar_01)
-	new_y_01 := barPool_02.Get()
-	if got, want := new_y_01.y, val_y_01; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	barPool03.Put(bar01)
+	newY01 := barPool02.Get()
+	if got, want := newY01.y, valY01; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	barPool_02.Put(bar_02)
-	new_y_02 := barPool_03.Get()
-	if got, want := new_y_02.y, val_y_02; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	barPool02.Put(bar02)
+	newY02 := barPool03.Get()
+	if got, want := newY02.y, valY02; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	barPool_01.Put(bar_03)
-	new_y_03 := barPool_03.Get()
-	if got, want := new_y_03.y, val_y_03; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	barPool01.Put(bar03)
+	newY03 := barPool03.Get()
+	if got, want := newY03.y, valY03; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
-	barPool_04 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
-	barPool_04.Get()             // pop new(T)
-	barPool_02.Put(new_y_03)
-	new_y_y3 := barPool_04.Get()
-	if got, want := new_y_y3.y, val_y_03; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	barPool04 := GetPool[Bar]() // when call GetPoo[T], it's call new (T) every time
+	barPool04.Get()             // pop new(T)
+	barPool02.Put(newY03)
+	newYy3 := barPool04.Get()
+	if got, want := newYy3.y, valY03; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 
 	// Should be the new
 	// value should be new
-	foo_x1 := fooPool_01.Get()
-	if got, want := foo_x1.x, 0; got != want {
-		t.Errorf("got: %d, want: %d", got, want)
+	fooX1 := fooPool01.Get()
+	if got, want := fooX1.x, 0; got != want {
+		t.Errorf(logTpl, got, want)
 	}
 }
+
+// // Function to drain the pool
+// func drainPool(pool *sync.Pool) {
+// 	for {
+// 		if pool.Get() == nil {
+// 			break
+// 		}
+// 	}
+// }
+
+// func TestSyncPoolReturnsNil(t *testing.T) {
+// 	type Dude struct{ x int }
+
+// 	dudePool01 := GetPool[Dude]()
+// 	pool := dudePool01.pool
+
+// 	// Ensure the pool is drained
+// 	drainPool(pool)
+
+// 	// Internal pull should be empty and return nil
+// 	// Then we must create new instead
+// 	item := dudePool01.Get()
+// 	if item == nil {
+// 		t.Fatalf("Expected nil, but got %v", item)
+// 	}
+// }
